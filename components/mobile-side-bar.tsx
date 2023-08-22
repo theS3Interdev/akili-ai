f88da471bef73bfc347ec038fa8c799a1e5c8eef
Michael Owuor <michael.owuor@gmail.com>
2023-08-23 02:29:13 +0300

    feat: implement basic side bar functionality (#5)

---
+++ components/mobile-side-bar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SideBar from "@/components/side-bar";
+
+const MobileSideBar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="left" className="p-0">
+        <SideBar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSideBar;
