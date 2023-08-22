98e0f70549c2942b278b78d5d425a05284456771
Michael Owuor <michael.owuor@gmail.com>
2023-08-22 15:13:29 +0300

    chore: folder setup (#1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/app/globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	icons: {
-		icon: '/favicon.svg',
-		shortcut: '/favicon/svg',
-	},
-	title: 'Akili AI Creativity Assistant',
-	description:
-		'Discover Akili AI: Your all-in-one AI creativity assistant. Create images, code snippets, and more with ease. Get started with 5 free generations and upgrade for unlimited access. Transform your workflow today!',
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon/svg",
+  },
+  title: "Akili AI Creativity Assistant | Akili AI",
+  description:
+    "Discover Akili AI: Your all-in-one AI creativity assistant. Create images, code snippets, and more with ease. Get started with 5 free generations and upgrade for unlimited access. Transform your workflow today!",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
