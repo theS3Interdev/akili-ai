881dc8cba9b4bdda360031e551d30b99ce1ca451
Michael Owuor <michael.owuor@gmail.com>
2023-08-22 18:31:48 +0300

    feat: implement clerk authentication (#3)

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div>
+      Landing Page - Unprotected
+      <div>
+        <Link href="/sign-in">
+          <Button>Sign In</Button>
+        </Link>
+
+        <Link href="/sign-up">
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
