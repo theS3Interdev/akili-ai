438217b734ccb856315820f5d1efda4b43f1096a
Michael Owuor <michael.owuor@gmail.com>
2023-08-24 12:54:25 +0300

    feat: implement code generation ai

--- components/loader.tsx
+++ components/loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-y-4">
       <div className="relative h-10 w-10 animate-spin">
-        <Image alt="Akili" src="/akili.png" fill />
+        <Image alt="Akili" src="/cog.png" fill />
       </div>
       <p className="text-sm text-muted-foreground">
         Akili is contemplating a response...
